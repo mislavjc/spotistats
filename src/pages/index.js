253128f868b66a6c88aa477b767768800504e9c2
Mislav <mislavjc@gmail.com>
2021-07-04 15:21:12 +0200

    added features section to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,53 @@
-import Link from 'next/link';
 import styles from '@/styles/Landing.module.scss';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className={styles.landingPage}>
-      <div>
-        <h1>Get your statics</h1>
-        <p>See what tracks were your most listened.</p>
-        <Link href="/top-tracks" passHref>
-          <button className="btn">START USING</button>
-        </Link>
+    <>
+      <div className={styles.landingPage}>
+        <div>
+          <h1>Get your statics</h1>
+          <p>See what tracks were your most listened.</p>
+          <Link href="/top-tracks" passHref>
+            <button className="btn">START USING</button>
+          </Link>
+        </div>
       </div>
-    </div>
+      <div className={styles.about}>
+        <div>
+          <h1>Statify features</h1>
+          <div className={styles.features}>
+            <div>
+              <div className={styles.icon}>
+                <Image src="/icons/music_note.svg" alt="music note" width={100} height={100} />
+              </div>
+              <div>
+                <h3>Most listened tracks.</h3>
+                <p>See the tracks you&apos;ve listened the most.</p>
+              </div>
+            </div>
+            <div>
+              <div className={styles.icon}>
+                <Image src="/icons/date_range.svg" alt="music note" width={100} height={100} />
+              </div>
+              <div>
+                <h3>Browse by period.</h3>
+                <p>Choose the time span for your listening habbits.</p>
+              </div>
+            </div>
+            <div>
+              <div className={styles.icon}>
+                <Image src="/icons/library_add.svg" alt="music note" width={100} height={100} />
+              </div>
+              <div>
+                <h3>Hassle-free playlist creation.</h3>
+                <p>Save your favourites to your library.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
